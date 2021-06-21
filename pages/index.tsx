a8d2aa7b752edfdcfff9544d0b8740b69494aac0
Braydon Coyer <braydon@coyer.net>
2021-06-21 08:58:02 -0500

    landing_page: Added SEO component with proper tags and OG image

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,19 +4,46 @@ import { Footer } from '../components/Footer'
 import { Hero } from '../components/Hero'
 import { PersonalNewsletter } from '../components/PersonalNewsletter'
 import Image from 'next/image'
+import { NextSeo } from 'next-seo'
 
 const IndexPage: NextPage<any> = () => {
   return (
-    <div className="min-h-screen flex flex-col">
-      <div className="flex-1">
-        <Hero />
-        <Image width="1800px" height="500" src="/pomegradient_app.svg" alt="Pomegradient App" />
-        <div className="max-w-10xl flex justify-center px-4">
-          <PersonalNewsletter />
+    <>
+      <NextSeo
+        title="Pomegradient"
+        description="Find, save and craft gradients with a creative community!"
+        canonical="https://www.pomegradient.com/"
+        openGraph={{
+          type: 'website',
+          url: 'https://www.pomegradient.com',
+          title: 'Pomegradient',
+          description: 'Find, save and craft gradients with a creative community!',
+          images: [
+            {
+              url: '/pomegradient_social_og.png',
+              width: 1200,
+              height: 627,
+              alt: 'Og Image Alt',
+            },
+          ],
+        }}
+        twitter={{
+          handle: '@pomeradient',
+          site: 'https://www.pomegradient.com',
+          cardType: 'summary_large_image',
+        }}
+      />
+      <div className="min-h-screen flex flex-col">
+        <div className="flex-1">
+          <Hero />
+          <Image width="1800px" height="500" src="/pomegradient_app.svg" alt="Pomegradient App" />
+          <div className="max-w-10xl flex justify-center px-4">
+            <PersonalNewsletter />
+          </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </>
   )
 }
 export default IndexPage
