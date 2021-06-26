66db87c0bfa3ebe7b737526b2d0999304372ef99
Braydon Coyer <braydon@coyer.net>
2021-06-26 16:30:27 -0500

    Continuing to work on detail page

--- pages/[username]/[slug].tsx
+++ pages/[username]/[slug].tsx
@@ -1,17 +1,29 @@
 import { NextPage } from 'next'
-import GradientContent from '../../components/GradientContent'
 import { Layout } from '../../components/Layout'
 import { firestore, getUserWithUsername, postToJSON } from '../../lib/firebase'
 import { useDocumentData } from 'react-firebase-hooks/firestore'
 import Heart from '../../components/HeartButton'
 import AuthCheck from '../../components/AuthCheck'
 import Link from 'next/link'
+import Highlight, { defaultProps } from 'prism-react-renderer'
+import React from 'react'
+import { GradientCardList } from '../../components/GradientCardList'
+import { HeaderComponent } from '../../components/Header'
 
 const GradientPage: NextPage<any> = (props) => {
   const gradientRef = firestore.doc(props.path)
+  const { moreGradients } = props
   const [realtimeGradient] = useDocumentData(gradientRef)
 
   const gradient = realtimeGradient || props.gradient
+
+  const cssCode = `
+    background: ${gradient.colors[0]};
+
+    background: -webkit-linear-gradient(to right, ${gradient.colors[0]}, ${gradient.colors[1]});  /* Chrome 10-25, Safari 5.1-6 */
+    
+    background: linear-gradient(to right, ${gradient.colors[0]}, ${gradient.colors[1]}); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    `
 
   return (
     <AuthCheck
@@ -26,63 +38,90 @@ const GradientPage: NextPage<any> = (props) => {
 
         {/* <GradientContent gradient={gradient} /> */}
         {/* <Heart gradientRef={gradientRef} /> */}
+        <HeaderComponent />
 
         <div className="md:flex md:space-x-12">
           <div
-            className="w-full h-64 rounded-xl"
+            className="w-full md:w-[300px] h-80 md:h-96 rounded-xl"
             style={{
               background: `linear-gradient(90deg, ${gradient.colors[0]} 0%, ${gradient.colors[1]} 100%)`,
             }}
           ></div>
-          <div className="mt-6">
-            <h1 className="text-3xl text-[#374151] font-extrabold">{gradient.name}</h1>
-            <p className="text-base text-[#9CA3AF]">By {gradient.username}</p>
-            <div className="space-y-6 mt-6">
-              <Heart gradientRef={gradientRef} heartCount={gradient.heartCount} />
-              <button
-                type="button"
-                className="w-full md:max-w-[200px] space-x-2 inline-flex items-center justify-center text-[#374151] font-medium bg-white bg-opacity-20 hover:bg-opacity-30 rounded-lg shadow-sm hover:shadow-lg py-3 px-5 border border-[#D1D5DB] transform hover:-translate-y-0.5 transition-all duration-150 md:flex"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
+          <div>
+            <div className="mt-6 md:flex md:items-end md:justify-between">
+              <div>
+                <h1 className="text-3xl text-[#374151] font-extrabold">{gradient.name}</h1>
+                <p className="text-base text-[#9CA3AF]">By {gradient.username}</p>
+              </div>
+              <div className="space-y-6 mt-6 md:space-x-4 md:space-y-0 md:flex md:justify-center md:items-center">
+                <Heart gradientRef={gradientRef} heartCount={gradient.heartCount} />
+                <button
+                  type="button"
+                  className="w-full md:max-w-[200px] space-x-2 inline-flex gradients-center justify-center text-[#374151] font-medium bg-white bg-opacity-20 hover:bg-opacity-30 rounded-lg shadow-sm hover:shadow-lg py-3 px-5 border border-[#D1D5DB] transform hover:-translate-y-0.5 transition-all duration-150 md:flex"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
-                  />
-                </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
+                    />
+                  </svg>
 
-                <span>Copy</span>
-              </button>
-              <button
-                type="button"
-                className="w-full md:max-w-[200px] space-x-2 inline-flex items-center justify-center text-[#374151] font-medium bg-white bg-opacity-20 hover:bg-opacity-30 rounded-lg shadow-sm hover:shadow-lg py-3 px-5 border border-[#D1D5DB] transform hover:-translate-y-0.5 transition-all duration-150 md:flex"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
+                  <span>Copy</span>
+                </button>
+                <button
+                  type="button"
+                  className="w-full md:max-w-[200px] space-x-2 inline-flex gradients-center justify-center text-[#374151] font-medium bg-white bg-opacity-20 hover:bg-opacity-30 rounded-lg shadow-sm hover:shadow-lg py-3 px-5 border border-[#D1D5DB] transform hover:-translate-y-0.5 transition-all duration-150 md:flex"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"
-                  />
-                </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"
+                    />
+                  </svg>
 
-                <span>Share</span>
-              </button>
+                  <span>Share</span>
+                </button>
+              </div>
+            </div>
+            <div>
+              <Highlight {...defaultProps} code={cssCode} language="css">
+                {({ className, style, tokens, getLineProps, getTokenProps }) => (
+                  <pre className={`${className} rounded-lg mt-6`} style={style}>
+                    {tokens.map((line, i) => (
+                      <div key={i} {...getLineProps({ line, key: i })}>
+                        {line.map((token, key, index) => (
+                          <span key="key" {...getTokenProps({ token, key, index })} />
+                        ))}
+                      </div>
+                    ))}
+                  </pre>
+                )}
+              </Highlight>
+              {/* <SyntaxHighlighter language="css" style={docco} wrapLongLines>
+              {cssCode}
+            </SyntaxHighlighter> */}
             </div>
           </div>
+        </div>
+        <div className="space-y-6 mt-6">
+          <h1 className="text-xl text-[#374151] font-extrabold">More by this creator</h1>
+          <GradientCardList gradients={moreGradients} />
         </div>
       </Layout>
     </AuthCheck>
@@ -96,16 +135,24 @@ export async function getStaticProps({ params }) {
 
   let gradient
   let path
+  let moreGradients
 
   if (userDoc) {
     const gradientRef = userDoc.ref.collection('gradients').doc(slug)
     gradient = postToJSON(await gradientRef.get())
 
+    const gradientQuery = userDoc.ref.collection('gradients').orderBy('createdAt', 'desc')
+
+    moreGradients = (await gradientQuery.get()).docs
+      .map(postToJSON)
+      .filter((x) => x.name !== gradient.name)
+      .splice(0, 4)
+
     path = gradientRef.path
   }
 
   return {
-    props: { gradient, path },
+    props: { gradient, path, moreGradients },
     revalidate: 10000,
   }
 }
